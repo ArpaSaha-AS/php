4af79b8f9119b36d24cdd1d70562abd4f8ff99a4
ArpaSaha-AS <85447920+ArpaSaha-AS@users.noreply.github.com>
2021-12-05 21:29:29 +0600

    Create variables.php

---
+++ php codes/variables.php
@@ -0,0 +1,16 @@
+<?php
+
+// Variables are containers for storing information
+// Starts with a $
+$name = "Arpa";
+// Variables are case sensitive
+$namE = "My Name is ";
+$income = 200.8 ;
+
+echo "This  is $name and her income is  $income dollar <br>";
+
+echo "$name is a good girl<br>";
+echo "$name is a WordPress Developer<br>";
+
+
+?>
